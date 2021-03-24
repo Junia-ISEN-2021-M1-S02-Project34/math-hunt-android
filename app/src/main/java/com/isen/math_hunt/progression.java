a97cc453960992d814cbd3e85ddfbac9c85b75e6
Alexandre <contact@bdoalex.com>
2021-03-24 11:29:34 +0100

    add 4th item

---
+++ app/src/main/java/com/isen/math_hunt/progression.java
@@ -0,0 +1,14 @@
+package com.isen.math_hunt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class progression extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progression);
+    }
+}
