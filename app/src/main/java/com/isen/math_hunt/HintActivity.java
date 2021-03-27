b57fe397c906e8bce4fd265c24cf200dcd8442d0
Lawaj <pierrick.dumas@isen.yncrea.fr>
2021-03-27 10:42:08 +0100

    ajout_fragment

--- app/src/main/java/com/isen/math_hunt/HintActivity.java
+++ app/src/main/java/com/isen/math_hunt/HintActivity.java
@@ -3,12 +3,27 @@ package com.isen.math_hunt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class HintActivity extends AppCompatActivity {
+
+
+    //private HintView enigmaHint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hint_activity);
     }
+
+    //TODO call dbb
+
+    /*
+    ArrayList<Enigma> enigmaArrayList = new ArrayList<>();
+        enigmaArrayList.add(new Enigma("Enigme 1", 50,"hint"));
+        enigmaArrayList.add(new Enigma("Enigme 2", 50, "hint"));
+        enigmaArrayList.add(new Enigma("Enigme 3", 50,"hint"));
+     */
 }
