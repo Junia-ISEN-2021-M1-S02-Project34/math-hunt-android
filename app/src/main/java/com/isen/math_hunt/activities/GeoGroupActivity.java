250f21efc9abde47dd4e9fc5c1ce634b2312f3f7
Alexandre <bdo.alex@gmail.com>
2021-03-28 20:16:07 +0200

    view done

--- app/src/main/java/com/isen/math_hunt/activities/GeoGroupActivity.java
+++ app/src/main/java/com/isen/math_hunt/activities/GeoGroupActivity.java
@@ -1,14 +1,16 @@
-package com.isen.math_hunt;
+package com.isen.math_hunt.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.isen.math_hunt.R;
 
 public class GeoGroupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_geo_group);
+        setContentView(R.layout.activity_geo_groupe);
     }
 }
