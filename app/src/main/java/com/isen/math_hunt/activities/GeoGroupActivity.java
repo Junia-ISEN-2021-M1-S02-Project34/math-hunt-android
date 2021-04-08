2a3cf1176688a77a5d3700395c4f382f5bb8b446
Pierre-Louis <pierre-louis.vallet@isen.yncrea.fr>
2021-04-08 15:39:05 +0200

    Geolocalisation setup v1
    récupération de la localisation

--- app/src/main/java/com/isen/math_hunt/activities/GeoGroupActivity.java
+++ app/src/main/java/com/isen/math_hunt/activities/GeoGroupActivity.java
@@ -1,21 +1,77 @@
 package com.isen.math_hunt.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.isen.math_hunt.R;
 
-public class GeoGroupActivity extends AppCompatActivity {
+import java.util.List;
+import java.util.Locale;
+
+public class GeoGroupActivity extends AppCompatActivity implements LocationListener{
+
+
+    private static final long LOCATION_REFRESH_TIME = 5000;
+    private static final float LOCATION_REFRESH_DISTANCE = 5;
+
+    private double latIsen = 50.63418574831333;
+    private double lonIsen = 3.048819125188862;
+
+    private double latFlandres = 50.636597029006495;
+    private double lonFlandres = 3.0694448466392066;
+
+    private int dist;
+
+
+    private Button button_location;
+    private TextView text_location;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo_groupe);
+
         Button geoGroupContinueButton = findViewById(R.id.geoGroupContinueButton);
+
+        text_location = findViewById(R.id.text_location);
+        button_location = findViewById(R.id.button_location);
+
+        if (ContextCompat.checkSelfPermission(GeoGroupActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(GeoGroupActivity.this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            },100);
+        }
+
+        button_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLocation();
+                text_location.setText("vous êtes à : " + dist + "m");
+
+            }
+        });
+
 
         geoGroupContinueButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,11 +79,71 @@ public class GeoGroupActivity extends AppCompatActivity {
                 switchActivity(EnigmaActivity.class);
             }
         });
+
+
+    }
+    // On recupere la geolocalisation avec cette fonction
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+
+        try {
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,LOCATION_REFRESH_TIME,LOCATION_REFRESH_DISTANCE,GeoGroupActivity.this);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
-    public void switchActivity(Class activity){
+
+    public void switchActivity(Class activity) {
         Intent myIntent = new Intent(this, activity);
         startActivity(myIntent);
-
     }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        dist = (int) distance(latFlandres,location.getLatitude(),lonFlandres,location.getLongitude());
+        //Toast.makeText(this, "vous êtes à : " + dist + "m", Toast.LENGTH_SHORT).show();
+        text_location.setText("vous êtes à : " + dist + "m");
+        try {
+            Geocoder geocoder = new Geocoder(GeoGroupActivity.this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+            String address = addresses.get(0).getAddressLine(0);
+
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Calculate distance between two points in latitude and longitude taking
+     * into account height difference. If you are not interested in height
+     * difference pass 0.0. Uses Haversine method as its base.
+     *
+     * lat1, lon1 Start point lat2, lon2 End point el1 Start altitude in meters
+     * el2 End altitude in meters
+     * @returns Distance in Meters
+     */
+    public static double distance(double lat1, double lat2, double lon1,
+                                  double lon2) {
+
+        final int R = 6371; // Radius of the earth
+        double height = 0;
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c * 1000; // convert to meters
+
+        distance = Math.pow(distance, 2) + Math.pow(height, 2);
+
+        return Math.sqrt(distance);
+    }
+
 }
--- app/src/main/java/com/isen/math_hunt/interfaces/Constant.java
+++ app/src/main/java/com/isen/math_hunt/interfaces/Constant.java
@@ -3,4 +3,5 @@ package com.isen.math_hunt.interfaces;
 public interface Constant {
 
     String URL_API = "https://math-hunt-api.herokuapp.com/api/";
+
 }
