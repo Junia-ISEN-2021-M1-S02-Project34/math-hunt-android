24bc2e6a369f3441a63d25bb78085433556aa390
Alexandre <bdo.alex@gmail.com>
2021-04-14 20:43:23 +0200

    rebase on dev

--- app/src/main/java/com/isen/math_hunt/adapters/HintAdapter.java
+++ app/src/main/java/com/isen/math_hunt/adapters/HintAdapter.java
@@ -1,81 +1,57 @@
 package com.isen.math_hunt.adapters;
 
 import android.content.Context;
-import android.media.Image;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.isen.math_hunt.R;
-import com.isen.math_hunt.entities.EnigmasProgression;
 import com.isen.math_hunt.entities.Hint;
-import com.isen.math_hunt.entities.Progression;
-import com.isen.math_hunt.entities.Team;
-import com.isen.math_hunt.model.RetrofitClient;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class HintAdapter extends ArrayAdapter<Hint> {
 
     private final Context mContext;
     private final List<Hint> hintList;
     private boolean showHintText = Boolean.FALSE;
-    private List<Progression> progressions;
-    private List<String> usedHintsIds;
-    private Team currentTeam;
-    private ImageView lockImageView;
-    private Hint currentHint;
-    private TextView hintTextView;
-    List<EnigmasProgression> enigmasProgressions;
 
 
-    public HintAdapter(@NonNull Context context, List<Hint> hints, List<String> usedHintsIds) {
+    public HintAdapter(@NonNull Context context, List<Hint> hints) {
         super(context, 0, hints);
         mContext = context;
         this.hintList = hints;
-        this.usedHintsIds = usedHintsIds;
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View listItem = convertView;
-        if (listItem == null)
-            listItem = LayoutInflater.from(mContext).inflate(R.layout.item_hint, parent, false);
+        if(listItem == null)
+            listItem = LayoutInflater.from(mContext).inflate(R.layout.item_hint,parent,false);
 
-        currentHint = hintList.get(position);
+        Hint currentHint = hintList.get(position);
 
-
-        TextView hintName = (TextView) listItem.findViewById(R.id.hintNameTextView);
+        TextView hintName = (TextView)listItem.findViewById(R.id.hintNameTextView);
         hintName.setText(currentHint.getName());
 
         TextView hintPenalty = (TextView) listItem.findViewById(R.id.hintPenaltyTextView);
         hintPenalty.setText(String.valueOf("Pénalité : " + currentHint.getPenalty()));
 
-        lockImageView = (ImageView) listItem.findViewById(R.id.lockImageView);
+        Button getHintButton = (Button)listItem.findViewById(R.id.getHintButton);
 
-
-        hintTextView = (TextView) listItem.findViewById(R.id.hintTextTextView);
+        TextView hintTextView = (TextView) listItem.findViewById(R.id.hintTextTextView);
         hintTextView.setVisibility(View.GONE);
 
 
-        hintTextView.setText(currentHint.getText());
-
-
-        lockImageView.setOnClickListener(new View.OnClickListener() {
+        getHintButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showHintText = !showHintText;
@@ -83,11 +59,10 @@ public class HintAdapter extends ArrayAdapter<Hint> {
             }
         });
 
-        
+        hintTextView.setText(currentHint.getText());
 
 
         return listItem;
     }
-
 
 }
