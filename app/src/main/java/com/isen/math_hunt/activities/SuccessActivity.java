3ef94d3c22fd8fb46c21452b0be8374553b4b33a
Alexandre <bdo.alex@gmail.com>
2021-04-22 00:46:38 +0200

    fix bug last enigma

--- app/src/main/java/com/isen/math_hunt/activities/SuccessActivity.java
+++ app/src/main/java/com/isen/math_hunt/activities/SuccessActivity.java
@@ -2,12 +2,14 @@ package com.isen.math_hunt.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.isen.math_hunt.R;
+import com.isen.math_hunt.entities.Login;
 
 public class SuccessActivity extends AppCompatActivity {
 
@@ -16,12 +18,16 @@ public class SuccessActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
 
+        Button leaveMathHuntButton = (Button) findViewById(R.id.leaveMathHuntButton);
+
+        leaveMathHuntButton.setOnClickListener(v -> {
+            Intent intent = new Intent(SuccessActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
 
     }
 
-    public void switchActivity(Class activity) {
-        Intent myIntent = new Intent(this, activity);
-        startActivity(myIntent);
 
-    }
 }
